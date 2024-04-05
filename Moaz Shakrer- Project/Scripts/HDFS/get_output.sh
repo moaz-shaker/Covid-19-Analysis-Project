a49d85ac01cc64dea918599ae4146eeacf34d0da
Moaz Shaker <Moazshaker121@gmail.com>
2024-04-05 03:33:48 +0200

    Covid-19 Analysis Project

--- Moaz Shakrer- Project/Scripts/HDFS/get_output.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-#Landing Zones in Linux and HDFS
-
-cat /home/cloudera/covid_project/header.csv /home/cloudera/covid_project_output/000000_0 > /home/cloudera/covid_project_output/covid-output.csv
--- Moaz Shakrer- Project/Scripts/Hive/remove-hive.hql
+++
@@ -1,5 +0,0 @@
-use covid_db;
-drop table if exists covid_staging;
-drop table if exists covid_ds_partitioned;
-drop table if exists covid_final_output;
-drop database covid_db;
